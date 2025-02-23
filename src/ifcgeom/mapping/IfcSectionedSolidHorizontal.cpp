berndhahnebach/IfcOpenShell

commit 26ba761c680495c53a11821721f30ee82fcb0409

Refactors alignment geometry

src/ifcgeom/taxonomy.cpp
@@ -1,7 +1,7 @@
 #include "../ifcparse/IfcLogger.h"
 #include "taxonomy.h"
 #include "profile_helper.h"
-#include "piecewise_function_impl.h"
+#include "function_item_evaluator.h"
 
 using namespace ifcopenshell::geometry::taxonomy;
 
@@ -163,9 +163,29 @@ namespace {
 		throw std::runtime_error("not implemented");
 	}
 
+    bool compare(const function_item&, const function_item&) {
+        throw std::runtime_error("not implemented");
+    }
+
+    bool compare(const functor_item&, const functor_item&) {
+        throw std::runtime_error("not implemented");
+    }
+
 	bool compare(const piecewise_function&, const piecewise_function&) {
 		throw std::runtime_error("not implemented");
 	}
+
+   bool compare(const gradient_function&, const gradient_function&) {
+        throw std::runtime_error("not implemented");
+   }
+
+   bool compare(const cant_function&, const cant_function&) {
+       throw std::runtime_error("not implemented");
+   }
+
+   bool compare(const offset_function&, const offset_function&) {
+       throw std::runtime_error("not implemented");
+   }
 
 	bool compare(const style& a, const style& b) {
 		const int order[5] = {
@@ -464,27 +484,58 @@ ifcopenshell::geometry::taxonomy::solid::ptr ifcopenshell::geometry::create_box(
 }
 
 ///////////////////
-piecewise_function::piecewise_function(double start, const spans_t& s, const IfcUtil::IfcBaseInterface* instance) : implicit_item(instance) {
-    impl_ = new piecewise_function_impl(start, s);
+piecewise_function::piecewise_function(double start, const spans_t& s, const IfcUtil::IfcBaseInterface* instance) : function_item(instance), start_(start), spans_(s) {
 }
 
-piecewise_function::piecewise_function(double start, const std::vector<piecewise_function::ptr>& pwfs, const IfcUtil::IfcBaseInterface* instance) : implicit_item(instance) {
-    impl_ = new piecewise_function_impl(start, pwfs);
+piecewise_function::piecewise_function(double start, const std::vector<piecewise_function::ptr>& pwfs, const IfcUtil::IfcBaseInterface* instance) : function_item(instance), start_(start) {
+    for (auto& pwf : pwfs) {
+        spans_.insert(spans_.end(), pwf->spans().begin(), pwf->spans().end());
+    }
 };
 
-piecewise_function::piecewise_function(const piecewise_function& other) : implicit_item(other) {
-    impl_ = other.impl_->clone_();
+const piecewise_function::spans_t& piecewise_function::spans() const { return spans_; }
+bool piecewise_function::is_empty() const { return spans_.empty(); }
+double piecewise_function::start() const { return start_; }
+double piecewise_function::end() const { return start_ + length(); }
+double piecewise_function::length() const {
+    return std::accumulate(spans_.begin(), spans_.end(), 0.0, [](const auto& v, const auto& s) { return v + s->length(); });
+
+    // this is a secondary option where we only compute length once and cache it.
+    // mutex is needed to prevent interruption of the accumulation if there is multi-threading
+    // skipping this detail for now and just adding up the span lengths every time
+    //if (!length_.has_value()) {
+    //    length_ = std::accumulate(spans_.begin(), spans_.end(), 0.0, [](const auto& v, const auto& s) { return v + s->length(); });
+    //}
+    //return *length_;
 }
 
-piecewise_function::~piecewise_function() {
-    delete impl_;
-}
 
-const piecewise_function::spans_t& piecewise_function::spans() const { return impl_->spans(); }
-bool piecewise_function::is_empty() const { return impl_->is_empty(); }
-double piecewise_function::start() const { return impl_->start(); }
-double piecewise_function::end() const { return impl_->end(); }
-double piecewise_function::length() const {   return impl_->length(); }
+gradient_function::gradient_function(piecewise_function::const_ptr horizontal, piecewise_function::const_ptr vertical, const IfcUtil::IfcBaseInterface* instance) : 
+	function_item(instance), horizontal_(horizontal), vertical_(vertical) {
+}
+double gradient_function::start() const { return std::max(horizontal_->start(), vertical_->start()); }
+double gradient_function::end() const { return std::min(horizontal_->end(), vertical_->end()); }
+piecewise_function::const_ptr gradient_function::get_horizontal() const { return horizontal_; }
+piecewise_function::const_ptr gradient_function::get_vertical() const { return vertical_; }
+
+
+cant_function::cant_function(gradient_function::const_ptr gradient, piecewise_function::const_ptr cant, const IfcUtil::IfcBaseInterface* instance) : 
+	function_item(instance), gradient_(gradient), cant_(cant) {
+}
+double cant_function::start() const { return std::max(gradient_->start(), cant_->start()); }
+double cant_function::end() const { return std::min(gradient_->end(), cant_->end()); }
+gradient_function::const_ptr cant_function::get_gradient() const { return gradient_; }
+piecewise_function::const_ptr cant_function::get_cant() const { return cant_; }
+
+
+offset_function::offset_function(function_item::const_ptr basis, piecewise_function::const_ptr offset, const IfcUtil::IfcBaseInterface* instance) : function_item(instance),
+                                                                                                                                                                       basis_(basis),
+                                                                                                                                                                       offset_(offset) {
+}
+double offset_function::start() const { return basis_->start(); }
+double offset_function::end() const { return basis_->end(); }
+function_item::const_ptr offset_function::get_basis() const { return basis_; }
+piecewise_function::const_ptr offset_function::get_offset() const { return offset_; }
 
 
 ifcopenshell::geometry::taxonomy::collection::ptr ifcopenshell::geometry::flatten(const taxonomy::collection::ptr& deep) {
@@ -499,7 +550,39 @@ const std::string& ifcopenshell::geometry::taxonomy::kind_to_string(kinds k) {
 	using namespace std::string_literals;
 
 	static std::string values[] = {
-		"matrix4"s, "point3"s, "direction3"s, "line"s, "circle"s, "ellipse"s, "bspline_curve"s, "offset_curve"s, "plane"s, "cylinder"s, "sphere"s, "torus"s, "bspline_surface"s, "edge"s, "loop"s, "face"s, "shell"s, "solid"s, "loft"s, "extrusion"s, "revolve"s, "sweep_along_curve"s, "node"s, "collection"s, "boolean_result"s, "piecewise_function"s, "colour"s, "style"s,
+        "matrix4"s,
+        "point3"s,
+        "direction3"s,
+        "line"s,
+        "circle"s,
+        "ellipse"s,
+        "bspline_curve"s,
+        "offset_curve"s,
+        "plane"s,
+        "cylinder"s,
+        "sphere"s,
+        "torus"s,
+        "bspline_surface"s,
+        "edge"s,
+        "loop"s,
+        "face"s,
+        "shell"s,
+        "solid"s,
+        "loft"s,
+        "extrusion"s,
+        "revolve"s,
+        "sweep_along_curve"s,
+        "node"s,
+        "collection"s,
+        "boolean_result"s,
+        "function_item"s,
+        "functor_item"s,
+        "piecewise_function"s,
+        "gradient_function"s,
+        "cant_function"s,
+        "offset_function"s,
+        "colour"s,
+        "style"s,
 	};
 
 	return values[k];
@@ -743,7 +826,7 @@ boost::optional<piecewise_function::ptr> ifcopenshell::geometry::taxonomy::loop_
 					axis = refDirection.cross(Y).normalized();
 					return make<matrix4>(o, axis, refDirection)->components();
 				};
-				spans.emplace_back(l, fn);
+            spans.emplace_back(taxonomy::make<taxonomy::functor_item>(l, fn));
 			}
 			pwf_ = make<piecewise_function>(0.0,spans);
 			loop_->pwf = pwf_;

src/ifcgeom/taxonomy.h
@@ -93,7 +93,50 @@ typedef item const* ptr;
 				topology_error(const char* const s) : std::runtime_error(s) {}
 			};
 
-			enum kinds { MATRIX4, POINT3, DIRECTION3, LINE, CIRCLE, ELLIPSE, BSPLINE_CURVE, OFFSET_CURVE, PLANE, CYLINDER, SPHERE, TORUS, BSPLINE_SURFACE, EDGE, LOOP, FACE, SHELL, SOLID, LOFT, EXTRUSION, REVOLVE, SWEEP_ALONG_CURVE, NODE, COLLECTION, BOOLEAN_RESULT, PIECEWISE_FUNCTION, COLOUR, STYLE };
+			// Implementer note: If you add a new item type, be sure to do the following
+			// 1) Add a new kind to this list
+			// 2) Update the values array used by kind_to_string()
+			// 3) Update the KindsTuple with the class name of the new item
+			// 4) Add compare function (see compare functions in taxonomy.cpp starting around line 9)
+			// 4) Update Python bindings
+			//    a) update list of assign_repr in IfcGeomWrapper.i
+			//    b) update inheritance list in IfcGeomWrapper.i (around line 120 in the file)
+			//    c) update item_to_pyobject function definition in type_conversion.i
+			enum kinds {
+                MATRIX4,
+                POINT3,
+                DIRECTION3,
+                LINE,
+                CIRCLE,
+                ELLIPSE,
+                BSPLINE_CURVE,
+                OFFSET_CURVE,
+                PLANE,
+                CYLINDER,
+                SPHERE,
+                TORUS,
+                BSPLINE_SURFACE,
+                EDGE,
+                LOOP,
+                FACE,
+                SHELL,
+                SOLID,
+                LOFT,
+                EXTRUSION,
+                REVOLVE,
+                SWEEP_ALONG_CURVE,
+                NODE,
+                COLLECTION,
+                BOOLEAN_RESULT,
+					 FUNCTION_ITEM,
+                FUNCTOR_ITEM,
+					 PIECEWISE_FUNCTION,
+                GRADIENT_FUNCTION,
+					 CANT_FUNCTION,
+					 OFFSET_FUNCTION,
+                COLOUR,
+                STYLE
+            };
 
 			const std::string& kind_to_string(kinds k);
 
@@ -358,25 +401,73 @@ typedef item const* ptr;
 				using geom_item::geom_item;
 			};
 
-			struct piecewise_function_impl; // forward declaration
-			struct piecewise_function : public implicit_item {
+			struct function_item : public implicit_item {
+                DECLARE_PTR(function_item)
+
+                function_item(const IfcUtil::IfcBaseInterface* instance = nullptr) : implicit_item(instance) {}
+                function_item(function_item&&) = default;
+                function_item(const function_item&) = default;
+
+                virtual ~function_item() = default;
+                virtual double start() const = 0;
+                virtual double end() const = 0;
+                virtual double length() const {
+                    return end() - start();
+                }
+
+                virtual kinds kind() const { return FUNCTION_ITEM; }
+                virtual size_t calc_hash() const {
+                    auto v = std::make_tuple(static_cast<size_t>(FUNCTION_ITEM), 0);
+                    return boost::hash<decltype(v)>{}(v);
+                };
+            };
+
+			struct functor_item : public function_item {
+            DECLARE_PTR(functor_item)
+
+            functor_item(double length, std::function<Eigen::Matrix4d(double u)> fn, const IfcUtil::IfcBaseInterface* instance = nullptr) : function_item(instance),
+						 length_(length), fn_(fn) {}
+            functor_item(functor_item&&) = default;
+            functor_item(const functor_item&) = default;
+            virtual ~functor_item() = default;
+
+				double start() const override { return 0.0; }
+            double end() const override { return length_; }
+
+				Eigen::Matrix4d operator()(double u) const { return fn_(u); }
+
+     			functor_item* clone_() const override { return new functor_item(*this); }
+            virtual kinds kind() const { return FUNCTOR_ITEM; }
+            virtual size_t calc_hash() const {
+                auto v = std::make_tuple(static_cast<size_t>(FUNCTOR_ITEM), 0);
+               return boost::hash<decltype(v)>{}(v);
+            }
+
+         private:
+				double length_;
+				std::function<Eigen::Matrix4d(double u)> fn_;
+         };
+
+			struct piecewise_function : public function_item {
             DECLARE_PTR(piecewise_function)
 
-				using spans_t = std::vector<std::pair<double, std::function<Eigen::Matrix4d(double u)>>>;
+				using spans_t = std::vector<function_item::const_ptr>;
 
             piecewise_function(double start, const spans_t& s, const IfcUtil::IfcBaseInterface* instance = nullptr);
             piecewise_function(double start, const std::vector<piecewise_function::ptr>& pwfs, const IfcUtil::IfcBaseInterface* instance = nullptr);
             piecewise_function(piecewise_function&&) = default;
-            piecewise_function(const piecewise_function&);
-            virtual ~piecewise_function();
+            piecewise_function(const piecewise_function&) = default;
+            virtual ~piecewise_function() = default;
 
 				const spans_t& spans() const;
+            size_t span_count() const {return spans_.size();}
+            function_item::const_ptr span_fn(size_t i) { return spans_[i]; }
 				bool is_empty() const;
-				double start() const;
-				double end() const;
-				double length() const;
+				double start() const override;
+				double end() const override;
+            double length() const override;
 
-				virtual piecewise_function* clone_() const { return new piecewise_function(*this); }
+				piecewise_function* clone_() const override { return new piecewise_function(*this); }
 				virtual kinds kind() const { return PIECEWISE_FUNCTION; }
 
 				virtual size_t calc_hash() const	{
@@ -385,11 +476,89 @@ typedef item const* ptr;
 				}
 
             private:
-				    // note: it would be better if this were a std::unique_ptr, but that requires having the full definition
-					 // of piecewise_function_impl in this header file, which defeats the purpose of the PIMPL idiom.
-					 // if this is a std::unique_ptr, then the _ifcopenshell_wrapper library doesn't compile
-                piecewise_function_impl* impl_ = nullptr;
-			};
+                double start_ = 0.0; // starting value of the pwf
+                spans_t spans_;
+            };
+
+         struct gradient_function : public function_item {
+             DECLARE_PTR(gradient_function)
+             gradient_function(piecewise_function::const_ptr horizontal, piecewise_function::const_ptr vertical, const IfcUtil::IfcBaseInterface* instance = nullptr);
+             gradient_function(gradient_function&&) = default;
+             gradient_function(const gradient_function&) = default;
+             virtual ~gradient_function() = default;
+
+               virtual double start() const override;
+               virtual double end() const override;
+
+					piecewise_function::const_ptr get_horizontal() const;
+               piecewise_function::const_ptr get_vertical() const;
+
+    				gradient_function* clone_() const override { return new gradient_function(*this); }
+
+               virtual kinds kind() const { return GRADIENT_FUNCTION; }
+
+               virtual size_t calc_hash() const {
+                  auto v = std::make_tuple(static_cast<size_t>(GRADIENT_FUNCTION), 0);
+                  return boost::hash<decltype(v)>{}(v);
+               }
+
+      		private:
+               piecewise_function::const_ptr horizontal_, vertical_;
+         };
+
+         struct cant_function : public function_item {
+             DECLARE_PTR(cant_function)
+             cant_function(gradient_function::const_ptr gradient, piecewise_function::const_ptr cant, const IfcUtil::IfcBaseInterface* instance = nullptr);
+             cant_function(cant_function&&) = default;
+             cant_function(const cant_function&) = default;
+             virtual ~cant_function() = default;
+
+             virtual double start() const override;
+             virtual double end() const override;
+
+				 gradient_function::const_ptr get_gradient() const;
+             piecewise_function::const_ptr get_cant() const;
+
+             cant_function* clone_() const override { return new cant_function(*this); }
+
+             virtual kinds kind() const { return CANT_FUNCTION; }
+
+             virtual size_t calc_hash() const {
+                 auto v = std::make_tuple(static_cast<size_t>(CANT_FUNCTION), 0);
+                 return boost::hash<decltype(v)>{}(v);
+             }
+
+           private:
+             gradient_function::const_ptr gradient_;
+             piecewise_function::const_ptr cant_;
+         };
+
+         struct offset_function : public function_item {
+             DECLARE_PTR(offset_function)
+             offset_function(function_item::const_ptr basis, piecewise_function::const_ptr offset, const IfcUtil::IfcBaseInterface* instance = nullptr);
+             offset_function(offset_function&&) = default;
+             offset_function(const offset_function&) = default;
+             virtual ~offset_function() = default;
+
+             virtual double start() const override;
+             virtual double end() const override;
+
+             function_item::const_ptr get_basis() const;
+             piecewise_function::const_ptr get_offset() const;
+
+             offset_function* clone_() const override { return new offset_function(*this); }
+
+             virtual kinds kind() const { return OFFSET_FUNCTION; }
+
+             virtual size_t calc_hash() const {
+                 auto v = std::make_tuple(static_cast<size_t>(OFFSET_FUNCTION), 0);
+                 return boost::hash<decltype(v)>{}(v);
+             }
+
+           private:
+             function_item::const_ptr basis_;
+             piecewise_function::const_ptr offset_;
+         };
 
 #ifdef TAXONOMY_USE_SHARED_PTR
 			typedef std::shared_ptr<item> ptr;
@@ -1041,7 +1210,7 @@ typedef item const* ptr;
 			};
 
 			namespace impl {
-				typedef std::tuple<matrix4, point3, direction3, line, circle, ellipse, bspline_curve, offset_curve, plane, cylinder, sphere, torus, bspline_surface, edge, loop, face, shell, solid, loft, extrusion, revolve, sweep_along_curve, node, collection, boolean_result, piecewise_function> KindsTuple;
+				typedef std::tuple<matrix4, point3, direction3, line, circle, ellipse, bspline_curve, offset_curve, plane, cylinder, sphere, torus, bspline_surface, edge, loop, face, shell, solid, loft, extrusion, revolve, sweep_along_curve, node, collection, boolean_result, function_item, functor_item, piecewise_function, gradient_function, cant_function,offset_function> KindsTuple;
 				typedef std::tuple<line, circle, ellipse, bspline_curve, offset_curve, loop, edge> CurvesTuple;
 				typedef std::tuple<plane, cylinder, sphere, torus, bspline_surface, extrusion, revolve> SurfacesTuple;
 				typedef std::tuple<edge, loop, face, piecewise_function> UpgradesTuple;

src/ifcwrap/IfcPython.i
@@ -171,7 +171,7 @@
 %{
 	#include "../ifcgeom/Iterator.h"
 	#include "../ifcgeom/taxonomy.h"
-	#include "../ifcgeom/piecewise_function_evaluator.h"
+	#include "../ifcgeom/function_item_evaluator.h"
 #ifdef IFOPSH_WITH_OPENCASCADE
 	#include "../ifcgeom/Serialization/Serialization.h"
 	#include "../ifcgeom/kernels/opencascade/IfcGeomTree.h"
@@ -265,7 +265,7 @@ constexpr bool is_std_vector_vector_v = is_std_vector_vector<T>::value;
 %module ifcopenshell_wrapper %{
 	#include "../ifcgeom/Converter.h"
 	#include "../ifcgeom/taxonomy.h"
-	#include "../ifcgeom/piecewise_function_evaluator.h"
+	#include "../ifcgeom/function_item_evaluator.h"
 #ifdef IFOPSH_WITH_OPENCASCADE
 	#include "../ifcgeom/Serialization/Serialization.h"
 	#include "../ifcgeom/kernels/opencascade/IfcGeomTree.h"

src/ifcgeom/AbstractKernel.cpp
@@ -3,7 +3,7 @@
 #include "../ifcgeom/IfcGeomElement.h"
 #include "../ifcgeom/ConversionSettings.h"
 #include "../ifcgeom/abstract_mapping.h"
-#include "../ifcgeom/piecewise_function_evaluator.h"
+#include "../ifcgeom/function_item_evaluator.h"
 
 #ifdef IFOPSH_WITH_OPENCASCADE
 #include "../ifcgeom/kernels/opencascade/OpenCascadeKernel.h"
@@ -71,6 +71,7 @@ bool is_valid_for_kernel(const ifcopenshell::geometry::kernels::AbstractKernel* 
 		return dynamic_cast<ifcopenshell::geometry::CgalShape*>(shp.Shape().get()) != nullptr;
 	}
 #endif
+    return false;
 }
 
 class HybridKernel : public ifcopenshell::geometry::kernels::AbstractKernel {
@@ -237,9 +238,44 @@ bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonom
 	return r.size() > s;
 }
 
-bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::piecewise_function::ptr item, IfcGeom::ConversionResults& cs) {
-   piecewise_function_evaluator evaluator(item);
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::function_item::ptr item, IfcGeom::ConversionResults& cs) {
+   function_item_evaluator evaluator(settings(),item);
    auto expl = evaluator.evaluate();
 	expl->instance = item->instance;
 	return convert(expl, cs);
+}
+
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::functor_item::ptr item, IfcGeom::ConversionResults& cs) {
+    function_item_evaluator evaluator(settings(), item);
+    auto expl = evaluator.evaluate();
+    expl->instance = item->instance;
+    return convert(expl, cs);
+}
+
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::piecewise_function::ptr item, IfcGeom::ConversionResults& cs) {
+    function_item_evaluator evaluator(settings(), item);
+    auto expl = evaluator.evaluate();
+    expl->instance = item->instance;
+    return convert(expl, cs);
+}
+
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::gradient_function::ptr item, IfcGeom::ConversionResults& cs) {
+    function_item_evaluator evaluator(settings(), item);
+    auto expl = evaluator.evaluate();
+    expl->instance = item->instance;
+    return convert(expl, cs);
+}
+
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::cant_function::ptr item, IfcGeom::ConversionResults& cs) {
+    function_item_evaluator evaluator(settings(), item);
+    auto expl = evaluator.evaluate();
+    expl->instance = item->instance;
+    return convert(expl, cs);
+}
+
+bool ifcopenshell::geometry::kernels::AbstractKernel::convert_impl(const taxonomy::offset_function::ptr item, IfcGeom::ConversionResults& cs) {
+    function_item_evaluator evaluator(settings(), item);
+    auto expl = evaluator.evaluate();
+    expl->instance = item->instance;
+    return convert(expl, cs);
 }

src/ifcgeom/AbstractKernel.h
@@ -76,7 +76,12 @@ namespace ifcopenshell {
 		virtual bool convert_impl(const taxonomy::sweep_along_curve::ptr, IfcGeom::ConversionResults&) { throw not_implemented_error(); }
 		virtual bool convert_impl(const taxonomy::loft::ptr, IfcGeom::ConversionResults&) { throw not_implemented_error(); }
 		virtual bool convert_impl(const taxonomy::collection::ptr, IfcGeom::ConversionResults&);
-		virtual bool convert_impl(const taxonomy::piecewise_function::ptr item, IfcGeom::ConversionResults& cs);
+		virtual bool convert_impl(const taxonomy::function_item::ptr item, IfcGeom::ConversionResults& cs);
+      virtual bool convert_impl(const taxonomy::functor_item::ptr item, IfcGeom::ConversionResults& cs);
+      virtual bool convert_impl(const taxonomy::piecewise_function::ptr item, IfcGeom::ConversionResults& cs);
+      virtual bool convert_impl(const taxonomy::gradient_function::ptr item, IfcGeom::ConversionResults& cs);
+      virtual bool convert_impl(const taxonomy::cant_function::ptr item, IfcGeom::ConversionResults& cs);
+      virtual bool convert_impl(const taxonomy::offset_function::ptr item, IfcGeom::ConversionResults& cs);
 
 		/*
 		virtual void set_offset(const std::array<double, 3> &p_offset);

src/ifcgeom/ConversionSettings.cpp
@@ -49,7 +49,7 @@ std::istream& ifcopenshell::geometry::settings::operator>>(std::istream& in, Ite
 	return in;
 }
 
-std::istream& ifcopenshell::geometry::settings::operator>>(std::istream& in, PiecewiseStepMethod& ioo) {
+std::istream& ifcopenshell::geometry::settings::operator>>(std::istream& in, FunctionStepMethod& ioo) {
     std::string token;
     in >> token;
     boost::to_upper(token);

commit 7ffd2742d65779494e4c7a415f04a133c1d0c45e

Fixes bug mapping geometry for IfcSectionedSolidHorizontal that was introduced in 26ba761

src/ifcgeom/mapping/IfcSectionedSolidHorizontal.cpp
@@ -31,10 +31,10 @@ taxonomy::ptr mapping::map_impl(const IfcSchema::IfcSectionedSolidHorizontal* in
 	std::vector<cross_section> cross_sections;
 
 	auto dir = map(inst->Directrix());
-	auto pwf = taxonomy::dcast<taxonomy::piecewise_function>(dir);
-	if (!pwf) {
+	auto fn = taxonomy::dcast<taxonomy::function_item>(dir);
+	if (!fn) {
 		// Only implement on alignment curves
-        Logger::Warning("IfcSectionedSolidHorizontal is only implemented for piecewise function Directrix curves", inst);
+        Logger::Warning("IfcSectionedSolidHorizontal is only implemented for Directrix curves based on taxonomy::function_item", inst);
         return nullptr;
 	}
 
@@ -70,9 +70,6 @@ taxonomy::ptr mapping::map_impl(const IfcSchema::IfcSectionedSolidHorizontal* in
 
 		profile_offsets.push_back(po);
 	}
-#else
-	return nullptr;
-#endif
 	if (faces.size() != profile_offsets.size()) {
 		Logger::Warning("Expected CrossSections and CrossSectionPositions to be equal length, but got " + std::to_string(faces.size()) + " and " + std::to_string(profile_offsets.size()) + " respectively", inst);
 		return nullptr;
@@ -85,9 +82,12 @@ taxonomy::ptr mapping::map_impl(const IfcSchema::IfcSectionedSolidHorizontal* in
 	for (size_t i = 0; i < faces.size(); ++i) {
 		cross_sections.push_back({ longitudes[i], faces[i], profile_offsets[i] });
 	}
-	}
+#else
+    return nullptr;
+#endif
+   }
 
-	return make_loft(settings_, inst, pwf, cross_sections);
+	return make_loft(settings_, inst, fn, cross_sections);
 }
 
 #endif

src/ifcgeom/taxonomy.cpp
@@ -800,13 +800,14 @@ boost::optional<face::ptr> ifcopenshell::geometry::taxonomy::curve_to_face_upgra
 }
 
 
-boost::optional<piecewise_function::ptr> ifcopenshell::geometry::taxonomy::loop_to_piecewise_function_upgrade_impl(ptr item) {
-	boost::optional<piecewise_function::ptr> pwf_;
+boost::optional<function_item::ptr> ifcopenshell::geometry::taxonomy::loop_to_function_item_upgrade_impl(ptr item) {
+	boost::optional<function_item::ptr> fi_;
 	auto loop_ = dcast<loop>(item);
 	if (loop_) {
-		if (loop_->pwf.is_initialized()) {
-			pwf_ = loop_->pwf;
+		if (loop_->fi.is_initialized()) {
+			fi_ = loop_->fi;
 		} else {
+         // piecewise_function is a specialization of function_item - callers don't need to know this detail
 			piecewise_function::spans_t spans;
 			spans.reserve(loop_->children.size());
 			for (auto& edge_ : loop_->children) {
@@ -828,9 +829,9 @@ boost::optional<piecewise_function::ptr> ifcopenshell::geometry::taxonomy::loop_
 				};
             spans.emplace_back(taxonomy::make<taxonomy::functor_item>(l, fn));
 			}
-			pwf_ = make<piecewise_function>(0.0,spans);
-			loop_->pwf = pwf_;
+			fi_ = make<piecewise_function>(0.0,spans);
+			loop_->fi = fi_;
 		}
 	}
-	return pwf_;
+    return fi_;
 }

src/ifcgeom/taxonomy.h
@@ -892,7 +892,7 @@ typedef item const* ptr;
 				DECLARE_PTR(loop)
 
 				boost::optional<bool> external, closed;
-				boost::optional<taxonomy::piecewise_function::ptr> pwf;
+				boost::optional<taxonomy::function_item::ptr> fi;
 
 				bool is_polyhedron() const {
 					for (auto& e : children) {
@@ -1374,27 +1374,27 @@ typedef item const* ptr;
 				}
 			};
 
-			boost::optional<piecewise_function::ptr> loop_to_piecewise_function_upgrade_impl(ptr item);
+			boost::optional<function_item::ptr> loop_to_function_item_upgrade_impl(ptr item);
             template <typename T>
-            class loop_to_piecewise_function_upgrade {
+            class loop_to_function_item_upgrade {
               private:
-                boost::optional<taxonomy::piecewise_function::ptr> pwf_;
+                boost::optional<taxonomy::function_item::ptr> fi_;
 
               public:
-               loop_to_piecewise_function_upgrade(taxonomy::ptr item) {
-					if constexpr (std::is_same_v<T, piecewise_function>) {
-						pwf_ = loop_to_piecewise_function_upgrade_impl(item);
+               loop_to_function_item_upgrade(taxonomy::ptr item) {
+					if constexpr (std::is_same_v<T, function_item>) {
+						fi_ = loop_to_function_item_upgrade_impl(item);
                     }
                 }
 
                 operator bool() const {
-                    return pwf_.is_initialized();
+                    return fi_.is_initialized();
                 }
 
                 operator typename T::ptr() const {
-					if constexpr (std::is_same_v<T, piecewise_function>) {
-						if (pwf_) {
-							return *pwf_;
+					if constexpr (std::is_same_v<T, function_item>) {
+						if (fi_) {
+							return *fi_;
 						}
 					}
 					return nullptr;
@@ -1435,7 +1435,7 @@ typedef item const* ptr;
 					}
 				}
 				{
-					loop_to_piecewise_function_upgrade<T> upg(u);
+					loop_to_function_item_upgrade<T> upg(u);
 					if (upg) {
 						return upg;
 					}
@@ -1479,7 +1479,7 @@ typedef item const* ptr;
 					}
 				}
 				{
-					loop_to_piecewise_function_upgrade<T> upg(u);
+					loop_to_function_item_upgrade<T> upg(u);
 					if (upg) {
 						return upg;
 					}

src/ifcwrap/IfcPython.i
@@ -86,7 +86,7 @@
 %ignore curve_to_loop_upgrade_impl;
 %ignore edge_to_loop_upgrade_impl;
 %ignore curve_to_face_upgrade_impl;
-%ignore loop_to_piecewise_function_upgrade_impl;
+%ignore loop_to_function_item_upgrade_impl;
 
 // settings, can this done more generally?
 %ignore UseElementNames;